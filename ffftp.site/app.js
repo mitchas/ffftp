c6c1aaf91ecb4295be6caf8cb550ac1ff98f603b
Mitch Samuels <mitch@mitchs.co>
2016-12-02 15:37:29 -0600

    Switched from github releases download to local zip download

--- ffftp.site/app.js
+++ ffftp.site/app.js
@@ -29,7 +29,7 @@ app.controller('downloadController', ['$scope', '$route', '$routeParams',
 function($scope, $route, $routeParams) {
     $scope.userVersion = $routeParams.version;
 
-    $scope.windowsDownload = 'https://github.com/mitchas/ffftp/tree/master/ffftp.app/releases/windows/'
+    $scope.windowsDownload = '/app/releases/ffftp-win-0.0.5.zip';
 
     $scope.changelog = [
         {
